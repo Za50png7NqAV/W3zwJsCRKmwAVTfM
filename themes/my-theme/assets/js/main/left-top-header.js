2f623c286b68f3ae77424cb3870f73008ed1e3d0
Za50png7Nq <adarshvinayakyp@gmail.com>
2025-04-27 20:32:21 +0530

    update post

---
+++ themes/my-theme/assets/js/main/left-top-header.js
@@ -0,0 +1,18 @@
+function toggleSidebar() {
+    document.getElementById('leftSidebar').classList.toggle('active');
+  }
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+    
+    // Close mobile menu when a nav item is clicked
+    document.querySelectorAll('.nav-menu a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
