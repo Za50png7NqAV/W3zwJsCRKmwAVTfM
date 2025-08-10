fe49164e321bb6f7281d18659e4f069e7cc079e7
Za50png7Nq <adarshvinayakyp@gmail.com>
2025-08-10 18:54:57 +0530

    update new Post

---
+++ themes/my-theme/assets/js/pages/whatsappstatus.js
@@ -0,0 +1,13 @@
+function getRandomQuote() {
+    const index = Math.floor(Math.random() * quotes.length);
+    return quotes[index].text;
+  }
+
+  window.onload = function() {
+    const quote = getRandomQuote();
+    const shareText = `$*{quote}*\n\n*Follow Yojna Portal*: https://wa.me/channel/0029VaCwEclFy72HhgQzJB0I`;
+    const encodedText = encodeURIComponent(shareText);
+
+    const whatsappURL = `https://wa.me/status?text=${encodedText}`;
+    window.location.href = whatsappURL;
+  }
