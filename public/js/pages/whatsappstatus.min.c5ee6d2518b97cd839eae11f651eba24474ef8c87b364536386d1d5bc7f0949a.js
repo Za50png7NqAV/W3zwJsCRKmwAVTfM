60d65347a4185baee0ea96220607d27016e7023a
Za50png7Nq <adarshvinayakyp@gmail.com>
2025-08-10 19:29:51 +0530

    update new post

---
+++ public/js/pages/whatsappstatus.min.c5ee6d2518b97cd839eae11f651eba24474ef8c87b364536386d1d5bc7f0949a.js
@@ -0,0 +1,3 @@
+function getRandomQuote(){const e=Math.floor(Math.random()*quotes.length);return quotes[e].text}window.onload=function(){const e=getRandomQuote(),t=`*${e}*
+
+*Follow Yojna Portal*: https://wa.me/channel/0029VaCwEclFy72HhgQzJB0I`,n=encodeURIComponent(t),s=`https://wa.me/status?text=${n}`;window.location.href=s}
